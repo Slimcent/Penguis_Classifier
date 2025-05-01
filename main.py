7b7082f69d11f75986b8c7e0c1ac662c1ac08b68
Slimcent <centpresley@gmail.com>
2025-05-01 15:23:41 +0100

    Trained the model.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import logging
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
 from fastapi import FastAPI
 from datetime import datetime
-import logging
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from logging.handlers import TimedRotatingFileHandler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Set up FastAPI app
 app = FastAPI()
