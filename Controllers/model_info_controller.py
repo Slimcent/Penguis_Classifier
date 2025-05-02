a5e1786d5651fc390bda0bfef49e25d066d79a43
Slimcent <centpresley@gmail.com>
2025-05-03 00:58:42 +0100

    Modified model info controller to use service response.

---
+++ Controllers/model_info_controller.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from Dtos.Response.model_response import ModelInfoResponse
+from Dtos.Response.service_response import ServiceResponse
+from Services.model_info_service import ModelInfoService
+
+router = APIRouter()
+model_info_service = ModelInfoService()
+
+
+@router.get("/penguin-info", summary="Penguin Model Info", description="Returns details about the training and model.",
+            tags=["Overview"], response_model=ServiceResponse[ModelInfoResponse])
+async def get_penguin_info():
+    return await model_info_service.get_model_info()
