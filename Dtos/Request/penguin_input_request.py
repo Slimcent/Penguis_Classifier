4b0c7d50e7bc84b748ce80bd0fdec372d75bc516
Slimcent <centpresley@gmail.com>
2025-05-03 12:15:19 +0100

    Created penguin download request.

--- Dtos/Request/penguin_input_request.py
+++ Dtos/Request/penguin_input_request.py
@@ -1,6 +1,8 @@
 from typing import List
-
 from pydantic import BaseModel
+from fastapi.params import File
+from fastapi import UploadFile, Form
+from Enums.file_type_enum import FileExportType
 
 
 class PenguinInputRequest(BaseModel):
@@ -10,3 +12,13 @@ class PenguinInputRequest(BaseModel):
 
 class BatchInputRequest(BaseModel):
     records: List[PenguinInputRequest]
+
+
+class DownloadPenguinPredictionsRequest:
+    def __init__(
+        self,
+        file: UploadFile = File(...),
+        file_type: FileExportType = Form(...)
+    ):
+        self.file = file
+        self.file_type = file_type
