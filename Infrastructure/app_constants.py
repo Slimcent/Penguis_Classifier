fbb78b9a85b660ff74a923029caff0a13b283714
Slimcent <centpresley@gmail.com>
2025-05-04 01:34:57 +0100

    Created app constants.

---
+++ Infrastructure/app_constants.py
@@ -0,0 +1,4 @@
+class AppConstants:
+    model_version = "v1.0"
+    prediction_storage_base = "penguin_predictions"
+    base_folder = "PredictionStorage"
---
+++ Infrastructure/app_constants_dependency.py
@@ -0,0 +1,5 @@
+from Infrastructure import app_constants
+
+
+def get_constants() -> app_constants:
+    return app_constants
