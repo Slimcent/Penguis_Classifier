b1628380d92e495336bd311d45250edc5b558c39
Slimcent <centpresley@gmail.com>
2025-05-04 21:25:54 +0100

    Created github app constants.

--- Infrastructure/app_constants.py
+++ Infrastructure/app_constants.py
@@ -2,3 +2,5 @@ class AppConstants:
     model_version = "v1.0"
     prediction_storage_base = "penguin_predictions"
     base_folder = "PredictionStorage"
+    github_csv_path = "Github_Prediction_Storage/predictions.csv"
+    github_excel_path = "Github_Prediction_Storage/predictions.xlsx"
