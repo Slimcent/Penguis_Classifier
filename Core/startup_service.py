d3547e1ba201706cee0e7a1711e7a322be18ff37
Slimcent <centpresley@gmail.com>
2025-05-06 12:59:50 +0100

    Added requirement text file file.

--- Core/startup_service.py
+++ Core/startup_service.py
@@ -1,3 +1,4 @@
+import os
 from Models.model_trainer import ModelTrainer
 
 
@@ -5,3 +6,9 @@ class StartupService:
     async def run(self):
         trainer = ModelTrainer()
         await trainer.train_model()
+
+    current_env = os.getenv("ENV", "prod")  # default to "prod"
+    if current_env == "dev":
+        print("Running in development mode.")
+    else:
+        print("Running in production.")
