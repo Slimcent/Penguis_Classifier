cdf691e590a2fc68d4387b216b994a90e8ed74d0
Slimcent <centpresley@gmail.com>
2025-05-09 16:29:11 +0100

    Called redis cache method in model info class.

--- Services/model_info_service.py
+++ Services/model_info_service.py
@@ -3,14 +3,27 @@ from Services.logger_service import LoggerService
 from Core.global_model_loader import model_loader
 from Dtos.Response.model_response import ModelInfoResponse
 from Dtos.Response.service_response import ServiceResponse
+from Services.redis_service import RedisService
+from Infrastructure.app_constants import AppConstants
 
 
 class ModelInfoService:
     def __init__(self):
         self.logger = LoggerService("prediction_service").get_logger()
+        self.constants = AppConstants
 
     async def get_model_info(self) -> ServiceResponse[ModelInfoResponse]:
         try:
+            # Get RedisService singleton instance
+            redis_service = await RedisService.get_instance()
+
+            # Try to get cached model info
+            cached = await redis_service.read_from_cache(self.constants.model_info_key, compressed=True)
+            if cached:
+                self.logger.info("Model info retrieved from cache.")
+                return ServiceResponse(success=True, message="Loaded from cache", data=ModelInfoResponse(**cached))
+
+            # Load model if not already loaded
             if not model_loader.is_loaded():
                 await model_loader.load_model()
 
@@ -18,10 +31,20 @@ class ModelInfoService:
             if not result:
                 return ServiceResponse(success=False, message="Model info is not available", data=None)
 
-            self.logger.info(f"\nModel info retrieved successfully: {json.dumps(result.model_dump(), indent=2)}")
+            # Save to cache (convert result to dictionary before storing)
+            model_info_dict = result.dict()
+            self.logger.info(f"Caching model info: {json.dumps(result.model_dump(), indent=2)} {model_info_dict}")
+
+            await redis_service.write_to_cache(
+                key=self.constants.model_info_key,
+                value=model_info_dict,
+                compress=True,
+                ex=self.constants.cache_expiry
+            )
+
+            self.logger.info("Model info loaded and cached.")
             return ServiceResponse(success=True, message="Model info loaded successfully", data=result)
 
         except Exception as ex:
-            error_msg = f"Error retrieving model info: {str(ex)}"
-            self.logger.error(error_msg)
-            return ServiceResponse(success=False, message=error_msg, data=None)
+            self.logger.error(f"Error retrieving model info: {ex}")
+            return ServiceResponse(success=False, message=str(ex), data=None)
